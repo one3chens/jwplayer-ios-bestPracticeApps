9900197e73122193c5a9fcb969fcb14d8817d564
Karim Mourra <karim@jwplayer.com>
2016-05-24 14:22:54 -0400

    switched to cocoapods

---
+++ JWBestPracticeApps/Pods/Headers/Public/google-cast-sdk/GoogleCast/GCKError.h
@@ -0,0 +1,1 @@
+../../../../google-cast-sdk/GoogleCastSDK-2.10.0-Release/GoogleCast.framework/Versions/A/Headers/GCKError.h
---
+++ JWBestPracticeApps/Pods/Target Support Files/Pods-JWRemoteController/Pods-JWRemoteController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JWRemoteController : NSObject
+@end
+@implementation PodsDummy_Pods_JWRemoteController
+@end
