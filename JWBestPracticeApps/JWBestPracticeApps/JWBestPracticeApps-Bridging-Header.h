fb5e8d7fa18fe0e005f46317ef0b9b06d3c7971f
JWP Admin <jwpadmin@JWPs-MacBook-Pro.local>
2016-09-14 21:10:38 -0400

    added Siri extension

---
+++ JWBestPracticeApps/JWBestPracticeApps/JWBestPracticeApps-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef JWBestPracticeApps_Bridging_Header_h
+#define JWBestPracticeApps_Bridging_Header_h
+
+#import "JWBasicVideoViewController.h"
+
+#endif /* JWBestPracticeApps_Bridging_Header_h */
---
+++ JWBestPracticeApps/JWBestPracticeApps/SwiftTest.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftTest.swift
+//  JWBestPracticeApps
+//
+//  Created by JWP Admin on 9/9/16.
+//  Copyright © 2016 Karim Mourra. All rights reserved.
+//
+
+//import JWBasicVideoViewController
+
+import Foundation
