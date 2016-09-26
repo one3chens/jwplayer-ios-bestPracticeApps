1ba60ba1a5cc4890310ae4632e7438f1e6010a46
Karim Mourra <karim@jwplayer.com>
2016-09-26 16:05:11 -0400

    set up FairPlay view controller

---
+++ JWBestPracticeApps/JWFairPlayDrm/JWFairPlayDrmViewController.swift
@@ -0,0 +1,13 @@
+//
+//  JWFairPlayDrmViewController.swift
+//  JWBestPracticeApps
+//
+//  Created by Karim Mourra on 9/26/16.
+//  Copyright © 2016 Karim Mourra. All rights reserved.
+//
+
+import Cocoa
+
+class JWFairPlayDrmViewController: JWBasicVideoViewController {
+
+}
